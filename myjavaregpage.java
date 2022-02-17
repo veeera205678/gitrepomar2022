a7c3a40797d462b59aa3e78baf75108ad2fb174e
veera205678 <veera205678@gmail.com>
2022-02-17 20:05:04 +0530

    my 100% completed

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -1,2 +1,10 @@
 this is myjava registration page
+<<<<<<< Updated upstream
 my manager  work has completed
+=======
+1st day of java line of code
+2nd day of java line of code
+3rd day of java line of code
+4th day of java line of code
+5th day of java line of code
+>>>>>>> Stashed changes
